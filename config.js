8cece8f76ea9c8e7a2dcb308d4ea9bcff8be5774
John Na <moalnaserr@gmail.com>
2026-07-15 20:31:21 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 // then run `npm run build` so deployment headers are regenerated for that origin.
 // Do NOT commit real credentials to version control.
 window.BAWSALA_CONFIG = Object.freeze({
-  SUPABASE_URL: '',
-  SUPABASE_PUBLISHABLE_KEY: '',
+  SUPABASE_URL: 'https://lkxzjwxwglknyszvnwbw.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_SGDhdvmTXjYCWXImTImajQ_zcfOoy56',
   TURNSTILE_SITE_KEY: '',
   SYNC_DEBOUNCE_MS: 1400
 });
